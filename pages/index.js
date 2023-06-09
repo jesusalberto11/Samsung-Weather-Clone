fca47b93cace01cedfb8822ea6804de01742130f
Jesus Alberto <jesusalbertomoralesrico@gmail.com>
2023-06-08 22:04:52 -0500

    TODO: Implement the rest of API calls and organize data

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,11 @@ import Head from "next/head";
 import Layout from "../components/layout";
 import CurrentWeatherWidget from "../components/CurrentWeatherWidget";
 import WeekWeatherWidget from "../components/WeekWeatherWidget";
+import axios from "axios";
 
-export default function Home() {
+export default function Home({ weatherData }) {
+  console.log(weatherData);
+
   return (
     <Layout home>
       <Head>
@@ -13,8 +16,40 @@ export default function Home() {
         <hr />
         <CurrentWeatherWidget />
         <br />
-        <WeekWeatherWidget />
+        <WeekWeatherWidget
+          forecastDays={weatherData?.daily.time}
+          maxTemps={weatherData?.daily.temperature_2m_max}
+          minTemps={weatherData?.daily.temperature_2m_min}
+        />
+        <div> aaea{weatherData.keys}</div>
+        <div>Its day: {weatherData?.current_weather.is_day}</div>
+        <div>Current Temp: {weatherData?.current_weather.temperature}</div>
+        <div>Windspeed {weatherData?.current_weather.windspeed}</div>
+        <div>Apprent temp: {weatherData?.daily.apparent_temperature_max} </div>
       </section>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  try {
+    const response = await axios.get(
+      `https://api.open-meteo.com/v1/forecast?latitude=10.40&longitude=-75.51&hourly=temperature_2m,relativehumidity_2m,apparent_temperature,rain,uv_index,is_day,shortwave_radiation&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,uv_index_max&current_weather=true&timezone=auto`
+    );
+
+    const weatherData = response.data;
+
+    return {
+      props: {
+        weatherData,
+      },
+    };
+  } catch (error) {
+    console.error("Error fetching weather data:", error);
+    return {
+      props: {
+        weatherData: null,
+      },
+    };
+  }
 }
